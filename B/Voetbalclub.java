a607c34aeb678e68c33a747be09fd998a5da5630
Hmed-S <74610841+Hmed-S@users.noreply.github.com>
2021-02-12 10:48:47 +0100

    Add files via upload
    files voor de test die je moet schrijven

---
+++ B/Voetbalclub.java
@@ -0,0 +1,59 @@
+package practicum2.B;
+
+public class Voetbalclub {
+    private String naam;
+    private int aantalGewonnen;
+    private int aantalGelijk;
+    private int aantalVerloren;
+
+
+    public Voetbalclub(String naam){
+        this.naam = naam;
+    }
+
+
+    public int getAantalGewonnen() {
+        return aantalGewonnen;
+    }
+
+    public int getAantalVerloren() {
+        return aantalVerloren;
+    }
+
+    public int getAantalGelijk() {
+        return aantalGelijk;
+    }
+
+    public int aantalPunten(){
+        int punten_gewonnen = this.aantalGewonnen *3;
+        return punten_gewonnen + this.aantalGelijk;
+    }
+
+    public int aantalGespeeld(){
+        return this.aantalGelijk + this.aantalGewonnen +this.aantalVerloren;
+
+    }
+
+
+    public void verwerkResultaat(char ch) {
+        if (ch == 'w')
+            aantalGewonnen = aantalGewonnen + 1;
+        if (ch == 'g')
+            aantalGelijk = aantalGelijk + 1;
+        if (ch == 'v')
+            aantalVerloren = aantalVerloren + 1;
+    }
+
+
+    public String toString() {
+        return    naam + " "
+                + aantalGespeeld() + " " +
+                getAantalGewonnen() + " " +
+                + getAantalGelijk() + " "
+                + getAantalVerloren() + " "+
+                aantalPunten();
+
+    }
+
+
+}
